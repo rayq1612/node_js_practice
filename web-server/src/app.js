17efd34772040f3121382b71c5b634af7c1c9845
rameshkamir <rameshkamir@gmail.com>
2024-07-13 21:52:47 +0300

    Update templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,34 +1,46 @@
-const path = require('node:path')
+const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectory = (path.join(__dirname), '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
-app.use(express.static(publicDirectory))
+app.set('views', viewsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
-        tempreture: '38,7'
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'help',
-        something: 'something'
+        helpText: 'This is some helpful text.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
     })
 })
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: "About",
-        info: "Some text"
-    })
 })
